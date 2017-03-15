30a0f432def432d95f26076070fff731cb50ae6d
Your Name <you@example.com>
2017-03-15 15:40:56 +0800

    first commit

---
+++ vue-animi/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/readme',
+            component: resolve => require(['../components/common/Home.vue'], resolve),
+            children:[
+                {
+                    path: '/',
+                    component: resolve => require(['../components/page/Readme.vue'], resolve)
+                },
+                {
+                    path: '/basetable',
+                    component: resolve => require(['../components/page/Basetable.vue'], resolve)
+                },
+                {
+                    path: '/vuetable',
+                    component: resolve => require(['../components/page/VueTable.vue'], resolve)     // vue-datasource组件
+                },
+                {
+                    path: '/baseform',
+                    component: resolve => require(['../components/page/baseForm.vue'], resolve)     // vue-datasource组件
+                },
+            ]
+        },
+        {
+            path: '/login',
+            component: resolve => require(['../components/page/Login.vue'], resolve)
+        },
+    ]
+})
+
